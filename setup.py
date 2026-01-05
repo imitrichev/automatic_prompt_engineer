e1a0ff3fcfb1603ce3698b3390e29e743d8d7bbb
imitrichev <ivan.mitrichev@gmail.com>
2026-01-06 02:04:11 +0300

    0.29

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     package_data={'automatic_prompt_engineer': ['configs/*']},
     install_requires=[
         'numpy',
-        'openai',
+        'openai<=0.29',
         'fire',
         'tqdm',
         'gradio',
